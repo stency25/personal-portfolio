1c60589abd89e31d70e83fef04786e7f27051204
stency Nabwire <sta.nabwire@gmail.com>
2026-09-12 13:09:16 +0300

    Fix ContactMessageController RequestMapping path from /contact to /api/contact

--- src/main/java/com/stency/personal_portfolio/controller/ContactMessageController.java
+++ src/main/java/com/stency/personal_portfolio/controller/ContactMessageController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/contact")
+@RequestMapping("/api/contact")
 public class ContactMessageController {
 
     private final ContactMessageService contactMessageService;
